756bdc499842fb9d14f8d54d8214b00461490b54
4EverStudent <mail@4everstudent.com>
2016-10-05 15:13:54 +0100

    Add code to buttons so they show or hide text view.

--- app/src/main/java/a4everstudent/showhidedemo/MainActivity.java
+++ app/src/main/java/a4everstudent/showhidedemo/MainActivity.java
@@ -1,17 +1,27 @@
 package a4everstudent.showhidedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-// TODO: 05-10-2016 2 buttons Hide and Show
-// TODO: 05-10-2016 1 text label that reacts to buttons
 
 public class MainActivity extends AppCompatActivity {
+ View textView;
+
+    public void showText(View view){
+        textView.setVisibility(textView.VISIBLE);
+    }
+
+    public void hideText(View view){
+        textView.setVisibility(textView.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textLabel);
 
     }
 }
