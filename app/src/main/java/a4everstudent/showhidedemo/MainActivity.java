ebf38243d1ac9e69ede0b4476711b7dbf5c83b77
4EverStudent <mail@4everstudent.com>
2016-10-05 14:50:20 +0100

    Initial commit

---
+++ app/src/main/java/a4everstudent/showhidedemo/MainActivity.java
@@ -0,0 +1,16 @@
+package a4everstudent.showhidedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// TODO: 05-10-2016 2 buttons Hide and Show
+// TODO: 05-10-2016 1 text label that reacts to buttons
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
